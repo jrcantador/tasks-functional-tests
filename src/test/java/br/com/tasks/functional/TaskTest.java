0c95ab7b94d71af91f40440c8ed1ccb002e50fc7
Júnior <jrcantador@gmail.com>
2022-10-09 12:51:19 -0300

    Add remote url chrome drive

--- src/test/java/br/com/tasks/functional/TaskTest.java
+++ src/test/java/br/com/tasks/functional/TaskTest.java
@@ -1,5 +1,6 @@
 package br.com.tasks.functional;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -7,11 +8,17 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TaskTest {
+	
+	public static String remote_url_chrome = "http://hib-1:4444/wd/hub";
 
 	public WebDriver acessarAplicacao() {
 		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		driver.set(new RemoteWebDriver(new URL(remote_url_chrome), options));
 		driver.navigate().to("http://tomcat:8000/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
